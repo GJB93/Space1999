f216283f79ad3f67f1ace5c0bcd5c107c712ef4b
Graham Byrne <grahambyrne1993@gmail.com>
2018-03-13 09:21:09 +0000

    Setup phase

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public Vector3 force = Vector3.zero;
+    public Vector3 acceleration = Vector3.zero;
+    public Vector3 velocity = Vector3.zero;
+    public float mass = 1.0f;
+    public float maxSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/EagleSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleSpawner : MonoBehaviour {
+
+    public float gap = 20;
+    public float followers = 2;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
