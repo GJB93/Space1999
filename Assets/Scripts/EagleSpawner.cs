324095918a6929f080be6fd5436386cfd13f0e98
Graham Byrne <grahambyrne1993@gmail.com>
2018-03-13 10:54:38 +0000

    Rotations incorrect, offset pursue is a little off

--- Assets/Scripts/EagleSpawner.cs
+++ Assets/Scripts/EagleSpawner.cs
@@ -9,15 +9,20 @@ public class EagleSpawner : MonoBehaviour {
     public GameObject prefab;
     
 	void Awake () {
-        GameObject leaderObject = Instantiate(prefab, transform.position + new Vector3(0, 0, 0), transform.rotation);
+        //GameObject leaderObject = Instantiate(prefab, transform.position + new Vector3(0, 0, 0), transform.rotation);
+        GameObject leaderObject = Instantiate(prefab, transform, false);
         GameObject target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         target.transform.position = leaderObject.transform.position + (leaderObject.transform.forward * 1000);
         leaderObject.AddComponent<Boid>();
         leaderObject.AddComponent<Seek>().targetGameObject = target;
         for(int i = 1; i <= followers; i += 1)
         {
-            GameObject followerLeft = Instantiate(prefab, transform.position + new Vector3((i * -20), 0, (i * 20)), transform.rotation);
-            GameObject followerRight = Instantiate(prefab, transform.position + new Vector3((i * 20), 0, (i * 20)), transform.rotation);
+            //GameObject followerLeft = Instantiate(prefab, transform.position + new Vector3((i * -20), 0, (i * 20)), transform.rotation);
+            //GameObject followerRight = Instantiate(prefab, transform.position + new Vector3((i * 20), 0, (i * 20)), transform.rotation);
+            GameObject followerLeft = Instantiate(prefab, transform, false);
+            GameObject followerRight = Instantiate(prefab, transform, false);
+            followerLeft.transform.position += new Vector3((i * -20), 0, (i * 20));
+            followerRight.transform.position += new Vector3((i * 20), 0, (i * 20));
             followerLeft.AddComponent<Boid>();
             followerRight.AddComponent<Boid>();
             followerLeft.AddComponent<OffsetPursue>().leader = leaderObject.GetComponent<Boid>();
